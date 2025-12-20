8ce086e1a26c2ed25bbf4b83f2b75353c395e1bf
zeynalovayxan70@gmail.com <zeynalovayxan70@gmail.com>
2025-12-20 15:30:46 +0400

    two ways binding with ngModel(2 variants with simple fields and signals)

--- firstAppSignals/src/app/app.ts
+++ firstAppSignals/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  imports: [FormsModule],
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('firstAppSignals');
+  enteredName = '';
+  enteredEmail = signal('')
 }
